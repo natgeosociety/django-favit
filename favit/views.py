0afda2b9a2a76b10d00412728b2912be08bf54aa
Alexander Pann <alexander.pann@gmail.com>
2015-03-15 08:52:03 +0100

    use builtin json

--- favit/views.py
+++ favit/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseBadRequest
-import simplejson
+import json
 
 from .models import Favorite
 
