8c36feada9c95a87a6fba133167d74c62e2b7a8e
Lomatus <lomatus@users.noreply.github.com>
2015-08-26 17:22:33 +0800

    Add support for django 1.8+
    Add support for django 1.8+

--- favit/views.py
+++ favit/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseBadRequest
-from django.utils import simplejson
+import json as simplejson
 
 from .models import Favorite
 
